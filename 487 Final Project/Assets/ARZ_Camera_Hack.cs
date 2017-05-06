d70824cba55df6afa70df4f20b25fa51ed5e319c
jake rodriguez <jake rodriguez>
2017-05-05 20:02:41 -0500

    Can Move Cameras Now

---
+++ 487 Final Project/Assets/ARZ_Camera_Hack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARZ_Camera_Hack : MonoBehaviour {
+
+	public string cameraView = "";
+	public GameObject stereoCameraLeft = null;
+	public GameObject stereoCameraRight = null;
+	public bool camerasReady = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (stereoCameraLeft == null || stereoCameraRight == null) {
+			GetCameraComponents ();
+		} else {
+			camerasReady = true;
+		}
+	}
+	public void MoveCamera (Transform camTransform, float translation, float rotation) {
+		camTransform.Translate(0, 0, translation);
+		camTransform.Rotate(0, rotation, 0);
+	}
+	public void MoveCameras (float translation, float rotation) {
+		MoveCamera (stereoCameraRight.transform, translation, rotation);
+		MoveCamera (stereoCameraLeft.transform, translation, rotation);
+	}
+	private void GetCameraComponents() {
+		foreach (GameObject camObject in GameObject.FindGameObjectsWithTag("MainCamera")) {
+			if (camObject.name == "StereoCameraLeft") {
+				stereoCameraLeft = camObject;
+				Debug.Log ("Found Cam Left");
+			} else if (camObject.name == "StereoCameraRight") {
+				stereoCameraRight = camObject;
+				Debug.Log ("Found Cam Right");
+			} 
+		}
+	}
+}
---
+++ 487 Final Project/Assets/ARZ_Player_Control.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ARZ_Player_Control : MonoBehaviour {
+
+	// Movement Settings
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+	// ARZ MAIN
+	private GameObject arzMain;
+	private ARZ_Camera_Hack arzCameraHack;
+
+	// Use this for initialization
+	void Start () {
+		arzMain = GameObject.FindGameObjectWithTag ("ARZ_MAIN");
+		arzCameraHack = arzMain.GetComponent<ARZ_Camera_Hack> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/********* GET INPUT *********/
+
+		float translation = Input.GetAxis ("Vertical") * speed;
+		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
+
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+
+		/********* END GET INPUT *********/
+
+		/********* HANDLE INPUT *********/
+
+		if (arzCameraHack.camerasReady) {
+			// Move Cameras
+			arzCameraHack.MoveCameras (translation, rotation);
+			// Move Player (which this is attached to)
+			// TODO
+			// GIVE PLAYER A BODY
+			// MAKE SURE PLAYER AND CAMERA MOVE THE SAME!
+			// MEANING, if a player runs into a wall, the camera should stop too!
+		}
+
+		/********* END HANDLE INPUT *********/
+
+	}
+}
