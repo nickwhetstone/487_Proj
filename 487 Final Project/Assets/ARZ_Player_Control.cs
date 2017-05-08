bf2ef37f1d955d59684f02e76c8fe2112c1a2196
jake rodriguez <jake rodriguez>
2017-05-07 19:43:19 -0500

    Rotating

--- 487 Final Project/Assets/ARZ_Player_Control.cs
+++ 487 Final Project/Assets/ARZ_Player_Control.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class ARZ_Player_Control : MonoBehaviour {
 	public bool camerasHookedUp = false;
-	public float last = 0;
-
 	public float speed = 10.0F;
 	public float rotationSpeed = 100.0F;
 
-	public float smooth = 0.4f;
-	public float inputValue;
-	public float newRotationX;
-	public float newRotationY;
-	public float newRotationZ;
-	public float sensitivity = 6;
+
+
+	public float smooth = 1f;
+	public float newRotation;
+	public float sensitivity = 30;
+
 	private Vector3 currentAcceleration, initialAcceleration;
 	public float baseSpeed = 3f;
 	public GameObject gun;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		initialAcceleration = Input.acceleration;
+		currentAcceleration = Vector3.zero;
 	}
-	
+
+		
 	// Update is called once per frame
 	void Update () {
 		if (!camerasHookedUp && TransitionManager.InVR) {
@@ -46,12 +46,12 @@ public class ARZ_Player_Control : MonoBehaviour {
 			}
 
 			// test ();
-			// UpdateMe();
+			UpdateMe();
 			Debug.Log("-------------");
 			Vector3 currentRotation = transform.localRotation.eulerAngles;
 
 			//  -Input.gyro.rotationRateUnbiased.x
-			transform.Rotate (0, -Input.gyro.rotationRateUnbiased.y * sensitivity,0);
+			// transform.Rotate (0, -Input..y * sensitivity,0);
 			// transform.Rotate (0, 0, );
 			float translation = 0;
 
@@ -68,34 +68,11 @@ public class ARZ_Player_Control : MonoBehaviour {
 			// test(currentRotation,Input.gyro.rotationRateUnbiased.x);
 		}
 	}
-	public void MovePlayer (Transform playerTransform, float translation, float rotation) {
-		playerTransform.Translate(0, 0, -translation);
-		playerTransform.Rotate(0, rotation, 0);
-	}
-	void test (Vector3 currentRotation, float val)
-	{
-		val = Mathf.Clamp (val, -0.4f, 0.4f);
-
-		// check if the transform is at its max rotations
-		// accelerometer y changes the x
-		float next = currentRotation.z - val;
-
-		// now that we have what we could possibly go to, check if it's within bounds.
-
-		if (next > 320 && next < 360) {
-			// good
-			// down
-			last = val;
-		} else if (next > 40 && next < 320) {
-			// no good
-			val = -last;
-		} else if (next < 40) {
-			// good
-			// up
-			last = val;
-		}
-		transform.Translate (0, 0, -val);
-
+	void UpdateMe () {
+		//pre-processing
+		currentAcceleration = Vector3.Lerp(currentAcceleration, Input.acceleration - initialAcceleration, Time.deltaTime/smooth);
+		newRotation = Mathf.Clamp(currentAcceleration.x * sensitivity, -1, 1);
+		transform.Rotate(0, newRotation, 0);
 	}
 	// See more at: http://www.theappguruz.com/blog/learn-to-use-accelerometer-in-unity-in-10-mins#sthash.irrTbjuG.dpuf
 }
